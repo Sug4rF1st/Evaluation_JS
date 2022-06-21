1f62f87a6b10013216825d38ff7b2f37ad53f8e9
floury.teddy1601@gmail.comMathyslouna22.:q:mq <floury.teddy1601@gmail.com>
2022-06-21 08:46:46 +0200

    exercice4

--- assets/exercice4/exercice4.js
+++ assets/exercice4/exercice4.js
@@ -1,28 +1,30 @@
-var PU = window.prompt("Saisir le prix unitaire : ");
-var QTECOM = window.prompt("Saisir la quantité commander : ");
-var TOT = PU*QTECOM;
-var PAP = (PU*QTECOM)+REM+PORT;
+var PU = parseInt(prompt("Saisir le prix unitaire :"));
+var QTECOM = parseInt(prompt("Saisir la quantité :"));
+var PAP = 0;
 var REM = 0;
 var PORT = 6;
+var TOT = PU*QTECOM;
 
-if (TOT<=100&&TOT>=200)
-{
-    REM = 5%
-    PORT = 6
-    alert("Le prix à payer est de : "+PAP+)
-
-
-if (TOT<=201&&TOT>=499)
-{
-    REM = 10%
-    PORT = 2%
-    alert("Le prix est de : "+PAP+)
+if(TOT >= 100 && TOT <= 200){
+  REM = (TOT * 5 / 100);
+}
+else if (TOT > 200) {
+  REM = (TOT * 10 / 100);
+}
+else {
+    REM = 0;
 }
 
-if (TOT<=500)
-{
-    REM = 10%
-    PORT = 0
-    alert("Le prix est de : "+PAP+)
+if ( (TOT-REM) < 500){
+    PORT = ((TOT-REM)*2/100);
 }
+else{
+    PORT = 0;
 }
+
+if (PORT != 0 && PORT < 6){
+    PORT = 6;
+}
+PAP = (TOT-REM)+PORT;
+
+alert("le prix a payer est de : "+PAP+"La remise est de : "+REM+" Le port est de :"+PORT);
